e9bea7f27fac29f7f0a984b25d2de5eb8af52089
Pamela Sera <evermoresecrets@gmail.com>
2026-01-18 11:02:33 -0500

    Annotation update

--- annotations/nocedit.exe/pseudocode/include/types/funcdefs/CSound_setFormat.h
+++ annotations/nocedit.exe/pseudocode/include/types/funcdefs/CSound_setFormat.h
@@ -7,5 +7,5 @@
 struct CSound;
 
 // Function Definition: CSound_setFormat
-typedef int (*CSound_setFormat)(struct CSound* this, int bits_per_sample, int channels, int sample_rate, int* out_buffer_size);
+typedef int (*CSound_setFormat)(struct CSound* this_ptr, int bits_per_sample, int channels, int sample_rate, int* out_buffer_size);
 
